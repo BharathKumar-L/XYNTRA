bd701b52e36d5b743b4a8d0192471df3f708b4f0
BharathKumar-L <220701043@rajalakshmi.edu.in>
2025-01-16 18:00:19 +0530

    team & contactus setup completed

--- src/common/contact/Contact.jsx
+++ src/common/contact/Contact.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from "react";
+import "./Contact.css";
+import webmaster from "../../assets/webmaster.png";
 
 const Contact = () => {
   return (
     <div>
-        Contact
+      <div className="contact-team">
+        <div className="contact-card">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="BHUVAANESH R"
+          />
+          <div>
+            <h2>Bhuvaanesh R</h2>
+            <a href="tel:+917339624803">📞 73396 24803</a>
+          </div>
+        </div>
+        <div className="contact-card">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="JODERICK SHERWIN"
+          />
+          <div>
+            <h2>Joderick Sherwin</h2>
+            <a href="tel:+917094944667">📞 70949 44667</a>
+          </div>
+        </div>
+        <div className="contact-card">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="DEEPIGA DHARSHINI C"
+          />
+          <div>
+            <h2>Deepiga Dharshini C</h2>
+            <a href="tel:+918220439219">📞 82204 39219</a>
+          </div>
+        </div>
+        <div className="contact-card">
+          <img src={webmaster} alt="BHARATH KUMAR L" />
+          <div>
+            <h2>Bharath Kumar L</h2>
+            <a href="tel:+916369106579">📞 63691 06579</a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/common/team/Team.jsx
+++ src/common/team/Team.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Team.css";
+import webmaster from "../../assets/webmaster.png";
+
 
 const Team = () => {
   return (
@@ -15,7 +17,7 @@ const Team = () => {
             <h2>Bhuvaanesh R</h2>
             <h3>CHAIRMAN</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
         <div className="card1">
@@ -27,7 +29,7 @@ const Team = () => {
             <h2>Joderick Sherwin</h2>
             <h3>VICE CHAIRMAN</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
         <div className="card1">
@@ -39,19 +41,19 @@ const Team = () => {
             <h2>Deepiga Dharshini C</h2>
             <h3>SECRETARY & TREASURER</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
         <div className="card1">
           <img
-            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            src={webmaster}
             alt="BHARATH KUMAR L"
           />
           <div>
             <h2>Bharath Kumar L</h2>
             <h3>WEBMASTER</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="https://www.linkedin.com/in/bharath-kumar-l-955304289/">Follow Account</a>
           </div>
         </div>
       </div>
@@ -66,7 +68,7 @@ const Team = () => {
             <h2>Roshini D</h2>
             <h3>WEB DEVELOPMENT</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
         <div className="card1">
@@ -78,7 +80,7 @@ const Team = () => {
             <h2>Joderick Sherwin</h2>
             <h3>MACHINE LEARNING</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
         <div className="card1">
@@ -90,7 +92,7 @@ const Team = () => {
             <h2>Amrutha B J</h2>
             <h3>APP DEVELOPMENT</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
         <div className="card1">
@@ -102,31 +104,298 @@ const Team = () => {
             <h2>Caroline Suja J S</h2>
             <h3>MEDIA MANAGEMENT</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
         <div className="card1">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
-            alt="Caroline Suja J S"
+            alt="Ragul Sowmiyanarayanan G K"
           />
           <div>
             <h2>Ragul Sowmiyanarayanan G K</h2>
             <h3>EVENT MANAGEMENT</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
         <div className="card1">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
-            alt="Caroline Suja J S"
+            alt="Nandha Kumar P"
           />
           <div>
             <h2>Nandha Kumar P</h2>
             <h3>DESIGN TEAM</h3>
             <p></p>
-            <a href="#">Follow Account</a>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+      </div>
+      <h1 className="my-4">CORE TEAM</h1>
+      <div className="team dl" id="ct">
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Nithish Rao P"
+          />
+          <div>
+            <h2>Nithish Rao P</h2>
+            <h3>WEB DEVELOPMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Varun G"
+          />
+          <div>
+            <h2>Varun G</h2>
+            <h3>WEB DEVELOPMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Suranjana S"
+          />
+          <div>
+            <h2>Suranjana S</h2>
+            <h3>MACHINE LEARNING</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Sri Harshavardhanan R"
+          />
+          <div>
+            <h2>Sri Harshavardhanan R</h2>
+            <h3>APP DEVELOPMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Deepthi P"
+          />
+          <div>
+            <h2>Deepthi P</h2>
+            <h3>CONTENT WRITER</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Dhanasree L P"
+          />
+          <div>
+            <h2>Dhanasree L P</h2>
+            <h3>EVENT MANAGER</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Bharath D"
+          />
+          <div>
+            <h2>Bharath D</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="AgishRaj R"
+          />
+          <div>
+            <h2>AgishRaj R</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Bharath B"
+          />
+          <div>
+            <h2>Bharath B</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Swathi G"
+          />
+          <div>
+            <h2>Swathi G</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Aathithya S K"
+          />
+          <div>
+            <h2>Aathithya S K</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Kamalesh S P"
+          />
+          <div>
+            <h2>Kamalesh S P</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Dinisha R"
+          />
+          <div>
+            <h2>Dinisha R</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Divya K"
+          />
+          <div>
+            <h2>Divya K</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Vishwa J"
+          />
+          <div>
+            <h2>Vishwa J</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Ramalingam S"
+          />
+          <div>
+            <h2>Ramalingam S</h2>
+            <h3>EVENT MANAGEMENT</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Bhuvaneshwari M"
+          />
+          <div>
+            <h2>Bhuvaneshwari M</h2>
+            <h3>EVENT MANAGEMENT - PR TEAM</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Himeshwar N"
+          />
+          <div>
+            <h2>Himeshwar N</h2>
+            <h3>EVENT MANAGEMENT - PR TEAM</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Jayadharsini M"
+          />
+          <div>
+            <h2>Jayadharsini M</h2>
+            <h3>EVENT MANAGEMENT - PR TEAM</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Gokula Sarathy PS"
+          />
+          <div>
+            <h2>Gokula Sarathy PS</h2>
+            <h3>DESIGN TEAM</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Farheen Tabassum H"
+          />
+          <div>
+            <h2>Farheen Tabassum H</h2>
+            <h3>DESIGN TEAM</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
+          </div>
+        </div>
+        <div className="card1">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+            alt="Srivarshni S"
+          />
+          <div>
+            <h2>Srivarshni S</h2>
+            <h3>DESIGN TEAM</h3>
+            <p></p>
+            <a target="_blank" href="#">Follow Account</a>
           </div>
         </div>
       </div>
