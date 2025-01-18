7e8c00b08ef1bbf0ed590779eb0752ff03a43fbb
BharathKumar-L <220701043@rajalakshmi.edu.in>
2025-01-18 22:12:33 +0530

    --

--- src/common/Registration/index.jsx
+++ src/common/Registration/index.jsx
@@ -1,6 +1,4 @@
 import React, { useEffect, useState } from "react";
-import logo from "../../assets/logo.png";
-import { Link, useNavigate } from "react-router-dom";
 import './Registration.css';
 
 const Registration = () => {
