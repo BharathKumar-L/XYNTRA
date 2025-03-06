139ede3cc2487b89eb3419d2e3ea13f0e3f3718f
BharathKumar-L <220701043@rajalakshmi.edu.in>
2025-03-06 08:56:53 +0530

    6/3

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,4 @@
-import React, { useState } from 'react';
-import TimelineObserver from "react-timeline-animation";
-import Timeline from '../../common/timeline';
-import Navbar from '../../common/navbar/navbar';
-import Hero from '../../common/hero/hero';
-import Footer from '../../common/footer/footer';
+import React from 'react';
 import Regtimer from '../../common/reg_timer/reg_timer';
 import FAQ from '../../common/faq';
 import Regbutton from '../../common/button/reg-button';
