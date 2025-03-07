910d7764d1ecfb13c723eccf13c3a548e6945ba7
BharathKumar-L <220701043@rajalakshmi.edu.in>
2025-03-07 23:41:03 +0530

    8/3

--- src/common/brochure/brochure.jsx
+++ src/common/brochure/brochure.jsx
@@ -1,21 +1,19 @@
 import React from "react";
 import './brochure.css'
-import brochure from '../../assets/broch.pdf'
-import abstract from '../../assets/Abstract-Template.docx'
 
 const Brochure = () => {
   return (
     <div className="file">
       <a
-        href={abstract}
-        download="XYNTRA-BROCHURE.pdf"
+        href="../../assets/Abstract-Template.docx"
+        download="ABSTRACT-TEMPLATE.docx"
         className="brochure-button"
       >
         Abstract <br /> Template
       </a>
       <a
-        href={brochure}
-        download="ABSTRACT-TEMPLATE.docx"
+        href="../../assets/broch.pdf"
+        download="XYNTRA-BROCHURE.pdf"
         className="brochure-button"
       >
         Brochure
