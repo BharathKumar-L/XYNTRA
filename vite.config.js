71c8d0288e69de2eb1bcb0259d371dc4cb5534bd
BharathKumar-L <220701043@rajalakshmi.edu.in>
2025-03-07 19:35:48 +0530

    7/3

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
     host: true,
     port: 5173,
-    strictPort: true, 
-    allowedHosts: "all" 
-  }
+    strictPort: true,
+    allowedHosts: "all",
+  },
+  build: {
+    outDir: "dist", 
+  },
+  base: "./",
 });
