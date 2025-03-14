b7b04300e644acb3f3f5153f8dc6ccdba2f58ae9
BharathKumar-L <220701043@rajalakshmi.edu.in>
2025-03-14 09:14:45 +0530

    14/3

--- src/common/nav/nav.jsx
+++ src/common/nav/nav.jsx
@@ -1,5 +1,9 @@
 import React, { useState,useEffect } from "react";
 import { Link } from "react-router-dom";
+import home from "../../assets/home.png"
+import timeline from "../../assets/timeline.png"
+import team from "../../assets/team.png"
+import registration from "../../assets/registration.png"
 import "./nav.css";
 
 const CircularNavbar = () => {
@@ -16,22 +20,22 @@ const CircularNavbar = () => {
         <ul className="cnav-items">
           <li className="cnav-item">
             <Link to="/" className="cnav-link">
-              🏠
+              <img src={home} alt="home" srcset="" />
             </Link>
           </li>
           <li className="cnav-item">
             <Link to="/time" className="cnav-link">
-              🕒
+            <img src={timeline} alt="timeline" srcset="" />
             </Link>
           </li>
           <li className="cnav-item">
             <Link to="/team" className="cnav-link">
-              👥
+            <img src={team} alt="team" srcset="" />
             </Link>
           </li>
           <li className="cnav-item">
             <Link to="/register" className="cnav-link">
-              📝
+            <img src={registration} alt="registration" srcset="" />
             </Link>
           </li>
         </ul>
