8088bf5959a9b833214c0f1f50140702275c16bb
BharathKumar-L <220701043@rajalakshmi.edu.in>
2025-03-30 11:01:16 +0530

    30/03

--- src/common/prize/prize.jsx
+++ src/common/prize/prize.jsx
@@ -9,38 +9,38 @@ const Prize = () => {
   return (
     <div className="prize-card">
       <h1 class="neon-text" >PRIZE POOL</h1>
-    <div className="prize">
-      <PixelCard className='sprize'>
-        <div className="prizee" >
-          <img
-            style={{ width: '225px' }}
-            src={second} className='prize-img' id='prize-s' alt="2nd prize" />
-          <h1 className='prize-title-silver'>
-            2nd Prize <p className='amt'>₹8,000</p>
-          </h1>
-        </div>
-      </PixelCard>
-      <PixelCard variant='gold' className='fprize'>
-        <div className="prizee">
-          <img
-            style={{ width: '225px' }}
-            src={first} className='prize-img' id='prize-f' alt="1st prize" />
-          <h1 className='prize-title'>
-            1st Prize <p className='amt'>₹15,000</p>
-          </h1>
-        </div>
-      </PixelCard>
-      <PixelCard variant='orange' className='tprize'>
-        <div className="prizee" >
-          <img
-            style={{ width: '225px' }}
-            src={third} className='prize-img' id='prize-t' alt="3rd prize" />
-          <h1 className='prize-title-bronze'>
-            3rd Prize <p className='amt'>₹5,000</p>
-          </h1>
-        </div>
-      </PixelCard>
-    </div>
+      <div className="prize">
+        <PixelCard className='sprize'>
+          <div className="prizee" >
+            <img
+              style={{ width: '225px' }}
+              src={second} className='prize-img' id='prize-s' alt="2nd prize" />
+            <h1 className='prize-title-silver'>
+              2nd Prize <p className='amt'>₹8,000</p>
+            </h1>
+          </div>
+        </PixelCard>
+        <PixelCard variant='gold' className='fprize'>
+          <div className="prizee">
+            <img
+              style={{ width: '225px' }}
+              src={first} className='prize-img' id='prize-f' alt="1st prize" />
+            <h1 className='prize-title'>
+              1st Prize <p className='amt'>₹15,000</p>
+            </h1>
+          </div>
+        </PixelCard>
+        <PixelCard variant='orange' className='tprize'>
+          <div className="prizee" >
+            <img
+              style={{ width: '225px' }}
+              src={third} className='prize-img' id='prize-t' alt="3rd prize" />
+            <h1 className='prize-title-bronze'>
+              3rd Prize <p className='amt'>₹5,000</p>
+            </h1>
+          </div>
+        </PixelCard>
+      </div>
     </div>
   )
 }
